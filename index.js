f161e8d92a1b3851cad1377a6c96932572f82e32
George Boots <ggboots.99@gmail.com>
2020-06-27 08:30:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function nowServing(line){
   if(!line.length){
     return "There is nobody waiting to be served!"
   }
-  return `Currently serving ${line.swift()}`
+  return `Currently serving ${line.swift()}.`
 }
 
 function takeANumber(line, name){
