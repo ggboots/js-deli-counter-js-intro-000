72fd6b9e0043960192a67a6370a4a43e46c01aef
George Boots <ggboots.99@gmail.com>
2020-06-27 08:26:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ function currentLine(line){
   }
 
   return `The line is currently: ${numbersAndNames.join(',')}`
-}
+};
 
 function nowServing(line){
   if(!line.length){
     return "There is nobody waiting to be served!"
   }
   return `Currently serving ${line.swift()}`
-}
+};
 
 function takeANumber(line, name){
   line.push(name)
   return `Welcome, ${name}. You are number ${line.length} in line.`
-}
+};
